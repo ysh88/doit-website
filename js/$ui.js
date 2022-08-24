7f1e06d22afcbe59a2d4ea73e5745bf081c75c6f
ysh88 <ysh88porcorosso@gmal.com>
2022-08-24 16:21:36 +0900

    8장 FAQ, 아코디언 메뉴 완성

--- js/$ui.js
+++ js/$ui.js
@@ -36,3 +36,8 @@ $(".book_roll li").click(function () {
     }
   });
 });
+
+$(".accordio_box ol li").click(function () {
+  $(".accordio_box ol li").removeClass("on");
+  $(this).addClass("on");
+})
